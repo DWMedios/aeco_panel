8100d0f55d3c722b28fadf1c31e91dda7562b805
franciscocamposchimal <camposfranciscoing@gmail.com>
2025-05-12 22:38:08 -0600

    feat: config deploy

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,17 @@ COPY package.json package-lock.json* ./
 RUN npm install --force esbuild@0.25.4 && \
     npm install
 
+# Copiar el archivo .env antes de construir
+COPY .env* ./
 COPY . .
 
 RUN npm run build
 
 FROM nginx:alpine
+# Copia los archivos construidos
 COPY --from=build-stage /app/dist/ /usr/share/nginx/html
+
+# Copia la configuración personalizada de Nginx
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+# Expone el puerto 80 y 443
+EXPOSE 80 443
--- src/utils/api/fetch.ts
+++ src/utils/api/fetch.ts
@@ -2,13 +2,16 @@ import { useAuth } from '../../hooks/useAuth'
 import { ApiParams } from '../../interfaces/api'
 import { useCallback } from 'react'
 
-const api = import.meta.env.VITE_API_BASE_URL
+const api = import.meta.env.VITE_API_BASE_URL || 'http://145.79.0.220:3000/api/v1'
 
 export const useFetchWithAuth = () => {
   const { logout, getToken } = useAuth()
 
   const fetchRequest = useCallback(
     async <T>({ url, method, headers, body }: ApiParams): Promise<T> => {
+      if (!api) {
+        throw new Error('API URL not configured. Please check environment variables.')
+      }
       try {
         const token = getToken()
 
